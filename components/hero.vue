56cf5208de2ac416f81a030530660da88a38d46a
BLACKHENZ29 <ayehenz29@gmail.com>
2025-01-28 10:04:46 +0100

    Adding to cart

--- components/hero.vue
+++ components/hero.vue
@@ -5,10 +5,8 @@
         <button @click="toggleCart"
             class="w-40 h-[50px] bg-gray-900 text-white rounded-md flex items-center justify-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                <path
-                    d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
             </svg>
-            Cart
         </button>
         <div v-if="isCartOpen" @click="toggleCart" class="fixed inset-0 bg-black/50 z-10"></div>
 
@@ -23,7 +21,7 @@
             <div class="p-4">
                 <div v-if="cartItems.length > 0">
                     <div v-for="item in cartItems" :key="item.id"
-                        class="flex justify-between items-center border-b py-2">
+                        class="flex justify-between items-center border-b py-2 text-black">
                         <p>{{ item.title }}</p>
                         <p>${{ item.price.toFixed(2) }}</p>
                     </div>
@@ -64,35 +62,6 @@ export default {
         toggleCart() {
             this.isCartOpen = !this.isCartOpen;
         },
-        addToCart(product) {
-            try {
-                const cart = JSON.parse(localStorage.getItem("cart")) || [];
-                const exists = cart.find((item) => item.id === product.id);
-
-                if (exists) {
-                    Toastify({
-                        text: "Product already exists in the cart",
-                        duration: 3000,
-                        backgroundColor: "orange",
-                    }).showToast();
-                } else {
-                    cart.push(product);
-                    localStorage.setItem("cart", JSON.stringify(cart));
-                    this.cartItems = cart;
-                    Toastify({
-                        text: "Product successfully added to cart",
-                        duration: 3000,
-                        backgroundColor: "green",
-                    }).showToast();
-                }
-            } catch (error) {
-                Toastify({
-                    text: "Sorry, an error occurred while adding your product to the cart. Kindly try again.",
-                    duration: 5000,
-                    backgroundColor: "red",
-                }).showToast();
-            }
-        },
         checkout() {
             this.isCheckingOut = true;
             setTimeout(() => {
@@ -102,6 +71,7 @@ export default {
                 Toastify({
                     text: "Checkout complete!",
                     duration: 3000,
+                    close: true,
                     backgroundColor: "blue",
                 }).showToast();
             }, 3000);
@@ -109,7 +79,14 @@ export default {
     },
     mounted() {
         const cart = JSON.parse(localStorage.getItem("cart")) || [];
-        this.cartItems = cart;
+        if (cart.length > 0) {
+            this.cartItems = cart;
+        }
     },
+    watch: {
+        cartItems(newCartItems) {
+            localStorage.setItem("cart", JSON.stringify(newCartItems));
+        }
+    }
 };
 </script>
--- components/products.vue
+++ components/products.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="w-full h-auto bg-gray-50 p-4 flex flex-wrap gap-10">
-    <div v-for="product in products" :key="product.id"
+    <div
+      v-for="product in products"
+      :key="product.id"
       class="product-card h-[225px] w-[22%] bg-white rounded-lg p-2 transform transition-all duration-300 ease-in-out hover:scale-105 hover:rotate-3 cursor-pointer border-1 border-gray-200"
-      @click="openModal(product)">
+      @click="openModal(product)"
+    >
       <img :src="product.image" alt="product image" class="h-[120px] w-full object-contain mb-2" />
       <h2 class="text-sm font-bold truncate">{{ product.title }}</h2>
       <p class="text-xs text-gray-500">{{ product.category }}</p>
@@ -10,7 +13,6 @@
         ${{ (product.price * 1.022).toFixed(2) }}
       </p>
     </div>
-
     <div v-if="selectedProduct" class="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
       <div class="bg-white rounded-lg p-6 w-[90%] max-w-md shadow-lg relative">
         <button @click="closeModal" class="absolute top-2 right-2 text-gray-500 hover:text-gray-800">
@@ -22,19 +24,29 @@
         <p class="text-base text-gray-800 mb-4">
           ${{ (selectedProduct.price * 1.022).toFixed(2) }}
         </p>
-        <svg class="w-8 text-gray-800 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" >
-          <path fill-rule="evenodd"
+        <svg
+          class="w-8 text-gray-800 cursor-pointer"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          @click="addToCart(selectedProduct)"
+        >
+          <path
+            fill-rule="evenodd"
             d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z"
-            clip-rule="evenodd" />
+            clip-rule="evenodd"
+          />
         </svg>
-
       </div>
     </div>
   </div>
 </template>
 
+
 <script>
 import axios from "axios";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
 export default {
   name: "Products",
@@ -50,6 +62,51 @@ export default {
     },
     closeModal() {
       this.selectedProduct = null;
+    },
+    addToCart(product) {
+      try {
+        const cart = JSON.parse(localStorage.getItem("cart")) || [];
+        const foundProduct = cart.some((item) => item.id === product.id);
+        const updatedProduct = { ...product, price: product.price * 1.022 };
+
+        if (foundProduct) {
+          Toastify({
+            text: "Product already exists inside the cart.",
+            duration: 3000,
+            close: true,
+            gravity: "top",
+            // position: "center",
+            position: "right",
+            style: {
+              background: "#f59e0b",
+            },
+          }).showToast();
+        } else {
+          cart.push(updatedProduct);
+          localStorage.setItem("cart", JSON.stringify(cart));
+          Toastify({
+            text: "Product added to cart.",
+            duration: 3000,
+            close: true,
+            gravity: "top",
+            position: "right",
+            style: {
+              background: "#10b981",
+            },
+          }).showToast();
+        }
+      } catch (error) {
+        Toastify({
+          text: "Sorry, an error occurred while adding your product to the cart. Kindly try again.",
+          duration: 3000,
+          close: true,
+          gravity: "top",
+          position: "right",
+          style: {
+            background: "#ef4444",
+          },
+        }).showToast();
+      }
     },
   },
   async mounted() {
